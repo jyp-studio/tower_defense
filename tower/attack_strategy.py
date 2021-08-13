39e3bb78bc0d046df50e3a24c049365a8fa7e652
ecfack <ecfack007@gmail.com>
2021-08-13 12:29:39 +0800

    add attack anime but not all is work

--- tower/attack_strategy.py
+++ tower/attack_strategy.py
@@ -33,82 +33,169 @@ class AttackStrategy(ABC):
 
 class SingleAttack(AttackStrategy):
     """attack an enemy once a time"""
+
     def attack(self, enemies: list, tower:Tower, cd_count:int)->int:
+        attack_animate_time = 10 #等同各攻擊動畫的max_current_sprites+1
+
+        exist_enemy = False
         if cd_count < tower.cd_max_count:
+            if cd_count - 1 == tower.cd_max_count-attack_animate_time:
+                
+                #確認有無敵人在圈內
+                for en in enemies:
+                    if in_range(en, tower):
+                        x, y = en.rect.center
+                        tower.throw(x, y - 100)
+                        exist_enemy = True
+                        break
+
+                if not exist_enemy:
+                    cd_count = tower.cd_max_count-attack_animate_time    #讓塔的cd維持在判斷可以開始攻擊動畫的時候
+        
             return cd_count
 
-        for en in enemies:
-            if in_range(en, tower):
-                x, y = en.rect.center
-                tower.throw(x, y)
-                en.health -= tower.damage
-                cd_count = 0
-                return cd_count
-        return cd_count
+        else:
+            for en in enemies:
+                if in_range(en, tower):
+                    x, y = en.rect.center
+                    tower.throw(x, y)
+                    en.health -= tower.damage
+                    cd_count = 0
+                    return cd_count
+            return cd_count
 
 
 class SingleSlowAttack(AttackStrategy):
     """attack an enemy once a time"""
+
     def attack(self, enemies: list, tower:Tower, cd_count:int)->int:
+        attack_animate_time = 4 #等同各攻擊動畫的max_current_sprites+1
+
+        exist_enemy = False
         if cd_count < tower.cd_max_count:
+            if cd_count - 1 == tower.cd_max_count-attack_animate_time:
+                
+                #確認有無敵人在圈內
+                for en in enemies:
+                    if in_range(en, tower):
+                        x, y = en.rect.center
+                        tower.throw(x, y - 100)
+                        exist_enemy = True
+                        break
+
+                if not exist_enemy:
+                    cd_count = tower.cd_max_count-attack_animate_time    #讓塔的cd維持在判斷可以開始攻擊動畫的時候
+        
             return cd_count
-
-        for en in enemies:
-            if en.name == "goblin":
-                previous_stride = 10
-            elif en.name == "orc":
-                previous_stride = 5
-            else:
-                previous_stride = 3
-
-            if in_range(en, tower):
-                en.health -= tower.damage
-                if en.stride > 2:
-                    en.stride -= 2
+        else:
+            for en in enemies:
+                if en.name == "goblin":
+                    previous_stride = 10
+                elif en.name == "orc":
+                    previous_stride = 5
                 else:
-                    en.stride = 0.5
-                cd_count = 0
-                return cd_count
-            else:
-                en.stride = previous_stride
+                    previous_stride = 3
 
-        return cd_count
+                if in_range(en, tower):
+                    if en.name != "fly":
+                        en.health -= tower.damage
+                    if en.stride > 2:
+                        en.stride -= 2
+                    else:
+                        en.stride = 0.5
+                    cd_count = 0
+                    return cd_count
+                else:
+                    en.stride = previous_stride
+
+            return cd_count
 
 
 class AOE(AttackStrategy):
     """attack all the enemy in range once a time"""
+
+    # attack_anime=BlueFlame(0,0)
+
     def attack(self, enemies: list, tower:Tower, cd_count:int)->int:
+        attack_animate_time = 10 #等同各攻擊動畫的max_current_sprites+1
+
+        exist_enemy = False
         if cd_count < tower.cd_max_count:
+            if cd_count - 1 == tower.cd_max_count-attack_animate_time:
+                
+                #確認有無敵人在圈內
+                for en in enemies:
+                    if in_range(en, tower):
+                        x, y = en.rect.center
+                        tower.throw(x, y - 100)
+                        exist_enemy = True
+
+                if not exist_enemy:
+                    cd_count = tower.cd_max_count-attack_animate_time    #讓塔的cd維持在判斷可以開始攻擊動畫的時候
+        
             return cd_count
 
-        for en in enemies:
-            if in_range(en, tower):
-                en.health -= tower.damage
-                cd_count = 0
-        return cd_count
+        else:
+            for en in enemies:
+                if in_range(en, tower):
+                    if en.name == "fly":
+                        pass
+                    elif en.name == "skull":
+                        en.health -= tower.damage*2
+                    else:
+                        en.health -= tower.damage
+                    cd_count = 0
+            return cd_count
 
 
 class AOESlowAttack(AttackStrategy):
+
+    # attack_anime=Magic(0,0)
+
     def attack(self, enemies: list, tower, cd_count)->int:
+        attack_animate_time = 10 #等同各攻擊動畫的max_current_sprites+1
+
+        exist_enemy = False
         if cd_count < tower.cd_max_count:
+            if cd_count - 1 == tower.cd_max_count-attack_animate_time:
+                
+                #確認有無敵人在圈內
+                for en in enemies:
+                    if in_range(en, tower):
+                        x, y = en.rect.center
+                        tower.throw(x, y - 100)
+                        exist_enemy = True
+
+                if not exist_enemy:
+                    cd_count = tower.cd_max_count-attack_animate_time    #讓塔的cd維持在判斷可以開始攻擊動畫的時候
+        
             return cd_count
 
-        for en in enemies:
-            attack_counter = 0
-            if in_range(en, tower):
-                en.health -= tower.damage
-                en.stride *= 0.8
-                attack_counter += 1
-            else:
-                en.stride *= 1.25 ** attack_counter
-        cd_count=0
-        return cd_count
+        else:
+            for en in enemies:
+                attack_counter = 0
+                if in_range(en, tower):
+
+                    if en.name == "orc":    #對orc加傷
+                        en.health -= tower.damage*2
+                    else:
+                        en.health -= tower.damage
+
+                    en.stride *= 0.8
+                    attack_counter += 1
+                else:
+                    en.stride *= 1.25 ** attack_counter
+            cd_count=0
+            return cd_count
 
 
 class Snipe(AttackStrategy):
+
+    # attack_anime=Lightning(0,0)
+
     def attack(self, enemies: list, tower, cd_count) -> int:
         
-        attack_animate_time = 9
+        attack_animate_time = 9 #等同各攻擊動畫的max_current_sprites+1
 
         exist_enemy = False
         if cd_count < tower.cd_max_count:
@@ -127,12 +214,22 @@ class Snipe(AttackStrategy):
         
             return cd_count
 
-        for en in enemies:
-            if in_range(en, tower):
-                en.health -= tower.damage * 5
-                cd_count = 0
-                break
-        return cd_count
+        else:
+            for en in enemies:
+                if in_range(en, tower):
+                    cd_count = 0
+                    if en.name == "boss":   #對boss傷害
+                        if en.health >= en.max_health * 0.05:
+                            en.health -= en.max_health * 0.2
+                            break
+                        else:
+                            en.health =0
+                            break
+                    else:   #對普通怪傷害
+                        en.health -= tower.damage * 3
+                        break
+
+            return cd_count
 
 
 
